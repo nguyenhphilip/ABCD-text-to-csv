3341ee2131aae37f32a1abf91d4ff20af2bafe8f
Phil <33665495+nguyenhphilip@users.noreply.github.com>
2019-10-08 11:45:23 -0400

    Update script to run more efficiently

--- 2.0ABCD_text_to_csv.R
+++ 2.0ABCD_text_to_csv.R
@@ -1,12 +1,20 @@
 if (!('dplyr' %in% installed.packages()[,"Package"]))  install.packages('dplyr')
 if (!('readr' %in% installed.packages()[,"Package"]))  install.packages('readr')
 if (!('here' %in% installed.packages()[,"Package"]))  install.packages('here')
+if (!('readr' %in% installed.packages()[,"Package"]))  install.packages('readr')
+if (!('vroom' %in% installed.packages()[,"Package"]))  install.packages('vroom')
+if (!('data.table' %in% installed.packages()[,"Package"]))  install.packages('data.table')
+if (!('fs' %in% installed.packages()[,"Package"]))  install.packages('fs')
 
 library(dplyr)
 library(readr)
 library(here)
+library(readr)
+library(vroom)
+library(data.table)
+library(fs)
 
-local_files <- list.files(pattern = ".txt")
+local_files <- fs::dir_ls(glob = "*.txt")
 
 # remove files not necessary for merge
 
@@ -21,12 +29,13 @@ if (length(which(grepl("ABCD_SST.txt",local_files))) > 0) local_files = local_fi
 if (length(which(grepl("ABCD_rest.txt",local_files))) > 0) local_files = local_files[-which(grepl("ABCD_rest.txt",local_files))]
 if (length(which(grepl("ABCD_NBACK.txt", local_files))) > 0) local_files = local_files[-which(grepl("ABCD_NBACK.txt", local_files))]
 
+
 csv_files <- lapply(local_files, function(i){
   tryCatch({
     print(paste("Import: ", gsub("*.txt$|.txt", "", i)))
-    a <- read_tsv(i, col_names = TRUE, col_types = NULL, quote = "", skip_empty_rows = FALSE, comment = "")
-    a = as.data.frame(sapply(a, function(x) gsub("\"", "", x)))
-    names(a) = as.list(sapply(names(a), function(x) gsub("\"","",x)))
+    a = vroom(file = i, delim = "\t", quote = "", comment = "", progress = T, altrep_opts = T)
+    a = as_tibble(as.data.table(sapply(a, function(x) gsub("\"", "", x))))
+    names(a) = as.character(sapply(names(a), function(x) gsub("\"", "", x)))
     # Drop first row - contains header information, which is already present in the Data Dictionary
     a = a[-1,] 
     a = droplevels(a)
@@ -35,7 +44,7 @@ csv_files <- lapply(local_files, function(i){
     a
   }, error = function(e) {
     print(e)
-    read.table(file = i, sep = '\t', header = TRUE)
+    read_table(i)
   })
 })
 
@@ -51,6 +60,12 @@ for (p in 1:length(csv_files)) {
   csv_files[[p]] = dt
 }
 
+# sapply(csv_files, function(f){
+#   if ("visit" %in% names(i)){
+#     print(csv_files$i)
+#     i$event
+#   }
+# })
 
 # There are some other columns that appear in more than one instrument. 
 # The last merge step would introduce duplicate columns if they remain in the data. 
@@ -86,12 +101,12 @@ for (p in 1:length(csv_files)) {
 
 # spreadsheet created from NIMH web API
 
-release_names_nda <- read.csv("abcd_instruments_v2.csv", row.names = NULL)
+release_names_nda <- read_csv(here("abcd_instruments_v2.csv"))
 
 # make a folder to put your files in
 
-if(!file.exists(here("2.0-ABCD-Release"))){
-  dir.create(here("2.0-ABCD-Release"))
+if(!file.exists(here("2.0-ABCD-Release-updated"))){
+  dir.create(here("2.0-ABCD-Release-updated"))
 }
 # convert .txt files to .csv
 
@@ -105,13 +120,12 @@ sapply(csv_files, function(i){
     file_name = short_name
   }
   tryCatch({
-    print(paste("Writing", short_name, ":", file_name))
-    if(!file.exists(here("2.0-ABCD-Release",category))){
-      dir.create(here("2.0-ABCD-Release", category))
+    print(paste("Writing", category, short_name, ":", file_name))
+    if(!file.exists(here("2.0-ABCD-Release-updated",category))){
+      dir.create(here("2.0-ABCD-Release-updated", category))
     }
-    write.csv(i, 
-              file = here("2.0-ABCD-Release", category, paste0(file_name, ".csv")),
-              row.names = FALSE)
+    write_csv(i, 
+              path = here("2.0-ABCD-Release-updated", category, paste0(file_name, ".csv")))
   }, error = function(e){
     print(e)
   })
@@ -119,8 +133,8 @@ sapply(csv_files, function(i){
 
 # and save in native r format
 
-if(!file.exists(here("2.0-ABCD-Release-R-format"))){
-  dir.create(here("2.0-ABCD-Release-R-format"))
+if(!file.exists(here("2.0-ABCD-Release-R-format-up"))){
+  dir.create(here("2.0-ABCD-Release-R-format-up"))
 }
 
 sapply(csv_files, function(i){
@@ -132,9 +146,9 @@ sapply(csv_files, function(i){
     file_name = short_name
   }
   tryCatch({
-    print(paste("Writing in native R format", short_name, ":", file_name))
+    print(paste("Writing in native R format-up", short_name, ":", file_name))
     saveRDS(i, 
-            file = here("2.0-ABCD-Release-R-format", paste0(file_name, ".Rds")))
+            file = here("2.0-ABCD-Release-R-format-up", paste0(file_name, ".Rds")))
   }, error = function(e){
     print(e)
   })
